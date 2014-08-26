bed86662613eb9f8e13fc218f2ff6350d09c4d20
Sameer Rahmani <lxsameer@gnu.org>
2014-08-26 11:03:37 +0430

    JSON rule bug fixed

--- app/models/contact_details.rb
+++ app/models/contact_details.rb
@@ -1,9 +1,9 @@
 class ContactDetails < ActiveRecord::Base
-    include  Faalis::Concerns::Authorizable
+  include  Faalis::Concerns::Authorizable
   @already_validate = false
 
   belongs_to :contact
-  belongs_to :field, :class_name => "ContactField"
+  belongs_to :field, :class_name => 'ContactField'
 
   validates :detail_field_id, :presence => true
   validates :detail_type, :presence => true
@@ -18,18 +18,21 @@ class ContactDetails < ActiveRecord::Base
       return
     end
 
-    print "----" * 1000
     if detail_field_id.nil?
       errors[:detail_field] = _("Field can not be empty.")
       return
     end
     rules = ContactField.find(detail_field_id).validation_rules
-    params = JSON.parse(rules).deep_symbolize_keys
-    @already_validate = true
-    begin
-      self.class.validates :detail_value, **params
-    rescue ArgumentError => e
-      errors[:error] = _("Field validation rules is not valid!")
+    unless rules.nil?
+      params = JSON.parse(rules).deep_symbolize_keys
+      @already_validate = true
+      begin
+        self.class.validates :detail_value, **params
+      rescue ArgumentError => e
+        errors[:error] = _("Field validation rules is not valid!")
+      end
+    else
+      @already_validate = true
     end
   end
 end
