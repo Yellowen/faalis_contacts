bf72d2085909fa442d91e21747b84956038ac600
Sameer Rahmani <lxsameer@gnu.org>
2014-08-27 18:59:53 +0430

    Complex query added temporarly

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,12 +1,12 @@
 class Contact < ActiveRecord::Base
   include  Faalis::Concerns::Authorizable
   has_many :details, :class_name => "ContactDetails"
+
   validates :first_name, :presence => true
   validates :last_name, :presence => true
   #validates_with ValueValidator
 
   def name
       "#{organization} - #{first_name} #{last_name}"
-
   end
 end
